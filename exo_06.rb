fd3ba309dc13dcbdc904e761f7231f24dad9b7e6
adelineady <favier.adeline@gmail.com>
2020-04-07 17:31:52 +0200

    exo_06.rb added

---
+++ exo_06.rb
@@ -0,0 +1,9 @@
+number_of_hours_worked_per_day = 10
+number_of_days_worked_per_week = 5
+number_of_weeks_in_THP = 11
+
+puts "Travail : #{number_of_hours_worked_per_day * number_of_days_worked_per_week * number_of_weeks_in_THP}"
+
+puts "Et en minutes ça fait : #{number_of_minutes_in_an_hour * number_of_hours_worked_per_day * number_of_days_worked_per_week * number_of_weeks_in_THP}"
+
+# Une erreur apparaît : la variable "number_of_minutes_in_an_hour" est indéfinie et aucune valeur de lui est attribuée, le résultat ne peut donc pas s'afficher
