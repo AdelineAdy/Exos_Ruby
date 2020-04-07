2eb2ed84cd635f262137e983af7fb1ccaa43c5fc
orauline <rauline.oceane@gmail.com>
2020-04-07 18:11:36 +0200

    exos07 added

---
+++ exo_07_b.rb
@@ -0,0 +1,5 @@
+puts "Bonjour, c'est quoi ton blase ?"
+print "> "
+# ce programme fait la même chose que le précédent, la différence réside dans le fait qu' on affiche ">" sans retour à la ligne, afin de signifier à l'utilisateur qu'il doit entrer un input
+user_name = gets.chomp
+puts user_name
---
+++ exo_07_c.rb
@@ -0,0 +1,4 @@
+user_name = gets.chomp
+puts user_name
+
+# ce programme fait la même chose que les 2 précédents sauf qu'il n'affiche rien à l'utilisateur (l'utilisateur doit deviner qu'il doit entrer un input) 
