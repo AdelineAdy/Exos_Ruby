330be686998c36ef7fc9b3d865d49e33dfc785e2
orauline <rauline.oceane@gmail.com>
2020-04-07 16:53:29 +0200

    exo04 added

---
+++ exo_04.rb
@@ -0,0 +1,8 @@
+=begin 
+Lorsqu'on lance le programme, on a l'erreur suivante qui s'affiche : 
+exo_04.rb:1: unterminated string meets end of file
+puts "Salut, ça farte ?
+
+Car il manque les " fermants à la fin de la phrase: "Salut, ça farte ? "
+=end 
+puts "Salut, ça farte ?
