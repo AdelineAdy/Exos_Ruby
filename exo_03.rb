8de5c8dde13080d3c217a590f3e64bacbad496f2
orauline <rauline.oceane@gmail.com>
2020-04-07 16:53:01 +0200

    ex03.rb added

---
+++ exo_03.rb
@@ -0,0 +1,5 @@
+puts "Bonjour, monde !"
+#puts "Et avec une voix sexy, ça donne : Bonjour, monde !"
+
+# Ajouter un "#" au début d'une ligne permet de la transformer en commentaire
+# (commentaire sur une seule ligne) 
