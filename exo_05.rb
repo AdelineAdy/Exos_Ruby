d64da675f9f992fe2d1a4ef25f648451354da0d5
adelineady <favier.adeline@gmail.com>
2020-04-07 17:10:13 +0200

    exo_05.rb added

---
+++ exo_05.rb
@@ -0,0 +1,41 @@
+# afficher la chaîne de caractères "On va compter le nombre d'heures de travail à THP"
+puts "On va compter le nombre d'heures de travail à THP"
+
+# afficher la chaîne de caractère avec le résultat d'un calcul
+puts "Travail : #{10 * 5 * 11}"
+
+# afficher la chaîne de caractères avec le résultat d'un calcul
+puts "En minutes ça fait : #{10 * 5 * 11 * 60}"
+
+# afficher la chaîne de caractères "Et en secondes ?"
+puts "Et en secondes ?"
+
+# afficher le résultat de l'opération suivante
+puts 10 * 5 * 11 * 60 * 60
+
+# afficher la chaîne de caractères sans donner le résultat du booléen
+puts "Est-ce que c'est vrai que 3 + 2 < 5 - 7 ?"
+
+#afficher le résulat du booléen (premier résultat est inférieur au deuxième résultat soit false)
+puts 3 + 2 < 5 - 7
+
+# afficher la chaîne de caractère avec le résultat d'un calcul
+puts "Ça fait combien 3 + 2 ? #{3 + 2}"
+
+# afficher la chaîne de caractère avec le résultat d'un calcul
+puts "Ça fait combien 5 - 7 ? #{5 - 7}"
+
+# afficher la chaîne de caractères "Ok, c'est faux alors !"
+puts "Ok, c'est faux alors !"
+
+# afficher la chaîne de caractères "C'est drôle ça, faisons-en plus :"
+puts "C'est drôle ça, faisons-en plus :"
+
+# afficher la chaîne de caractère avec le résultat d'un booléen
+puts "Est-ce que 5 est plus grand que -2 ? #{5 > -2}"
+
+# afficher la chaîne de caractère avec le résultat d'un booléen
+puts "Est-ce que 5 est supérieur ou égal à -2 ? #{5 >= -2}"
+
+# afficher la chaîne de caractère avec le résultat d'un booléen
+puts "Est-ce que 5 est inférieur ou égal à -2 ? #{5 <= -2}"
